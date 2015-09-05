8c90c93695a2e3355177a7df851346d256bb501d
Fabien Garcia <fab0670312047@gmail.com>
2015-09-05 18:48:04 +0200

    optimise build

--- config.rb
+++ config.rb
@@ -105,10 +105,11 @@ end
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-
+  activate :minify_html
   # Minify Javascript on build
   activate :minify_javascript
 
+  activate :imageoptim
   # Enable cache buster
   # activate :asset_hash
 
