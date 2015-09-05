cf86ba61e754ec6102bef888e59d5f1b9737f1dd
Fabien Garcia <fab0670312047@gmail.com>
2015-09-05 17:33:45 +0200

    Init project

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'http://rubygems.org'
+
+gem 'middleman', '~> 3.4.0'
+gem 'middleman-blog', '~> 3.5.3'
+gem 'middleman-livereload', '~> 3.3.4'
+
+# For feed.xml.builder
+gem 'builder', '~> 3.2.2'
+
+# Code syntax highlighting
+gem 'middleman-syntax', '~> 2.0.0'
+gem 'redcarpet', '~> 3.1.2'
+
+# For "summary"-Helper
+gem 'nokogiri', '~> 1.6.6.2'
+gem 'sanitize', '~> 3.0.0'
+
+# For Image resize
+gem 'middleman-simple-thumbnailer'
+gem 'middleman-minify-html'
+gem 'middleman-deploy'
---
+++ config.rb
@@ -0,0 +1,113 @@
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "blog"
+
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "articles/{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tag/{tag}.html"
+  # blog.layout = "layout"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  # blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+set :casper, {
+  blog: {
+    url: 'http://fabfight.com',
+    name: 'Fabfight',
+    description: 'Talk less, fight more',
+    date_format: '%d %B %Y',
+    navigation: true,
+    logo: nil # Optional
+  },
+  author: {
+    name: 'Fabien',
+    bio: 'Fighter and cook by night, dev by day', # Optional
+    location: 'Berlin', # Optional
+    website: nil, # Optional
+    gravatar_email: 'fab0670312047@gmail.com' # Optional
+  },
+  navigation: {
+    "Home" => "/"
+  }
+}
+
+page '/feed.xml', layout: false
+page '/sitemap.xml', layout: false
+
+ignore '/partials/*'
+
+ready do
+  blog.tags.each do |tag, articles|
+    proxy "/tag/#{tag.downcase.parameterize}/feed.xml", '/feed.xml', layout: false do
+      @tagname = tag
+      @articles = articles[0..5]
+    end
+  end
+
+  proxy "/author/#{blog_author.name.parameterize}.html", '/author.html', ignore: true
+end
+
+
+# Reload the browser automatically whenever files change
+activate :livereload
+
+# Pretty URLs - http://middlemanapp.com/basics/pretty-urls/
+activate :directory_indexes
+
+# Middleman-Syntax - https://github.com/middleman/middleman-syntax
+set :haml, { ugly: true }
+set :markdown_engine, :redcarpet
+set :markdown, fenced_code_blocks: true, smartypants: true
+activate :syntax, line_numbers: false
+
+# Methods defined in the helpers block are available in templates
+# helpers do
+#   def some_helper
+#     "Helping"
+#   end
+# end
+
+set :css_dir, 'stylesheets'
+
+set :js_dir, 'javascripts'
+
+set :images_dir, 'images'
+
+set :partials_dir, 'partials'
+
+# Build-specific configuration
+configure :build do
+  # For example, change the Compass output style for deployment
+  activate :minify_css
+
+  # Minify Javascript on build
+  activate :minify_javascript
+
+  # Enable cache buster
+  # activate :asset_hash
+
+  # Use relative URLs
+  # activate :relative_assets
+
+  # Or use a different image path
+  # set :http_prefix, "/Content/images/"
+end
